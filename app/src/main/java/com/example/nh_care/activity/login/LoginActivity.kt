ceccef5fab61f41d1798114849fa9f5a3f2d4197
MDaniDarmawan <darmawandani1922@gmail.com>
2023-11-26 07:25:31 +0700

    update login

--- app/src/main/java/com/example/nh_care/activity/login/LoginActivity.kt
+++ app/src/main/java/com/example/nh_care/activity/login/LoginActivity.kt
@@ -13,6 +13,7 @@ import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.example.nh_care.R
 import com.example.nh_care.activity.MainActivity
+import com.example.nh_care.activity.register.RegisterActivity
 import com.example.nh_care.databinding.ActivityLoginBinding
 import com.example.nh_care.database.DbContract
 import com.example.nh_care.ui.beranda.BerandaFragment
@@ -25,82 +26,57 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var InputPass: EditText
     private lateinit var loginButton: Button
 
+    private lateinit var binding: ActivityLoginBinding
+    private val etUsername: EditText by lazy { binding.etEmail }
+    private val etPassword: EditText by lazy { binding.etKataSandi }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
 
-        InputEmail = findViewById(R.id.et_email)
-        InputPass = findViewById(R.id.et_kata_sandi)
-        loginButton = findViewById(R.id.tombol_masuk)
+        binding.tombolMasuk.setOnClickListener {
+            val username = etUsername.text.toString()
+            val password = etPassword.text.toString()
 
-        loginButton.setOnClickListener {
-            val email = InputEmail.text.toString()
-            val password = InputPass.text.toString()
-
-            if (email == "user" && password == "1234") {
-                // Login berhasil, alihkan ke aktivitas lain (misalnya, MainActivity)
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
-
+            if (!(username.isEmpty() || password.isEmpty())) {
+                loginUser(username, password)
             } else {
-                // Login gagal, berikan pesan kesalahan
+                Toast.makeText(applicationContext, "Fields cannot be empty", Toast.LENGTH_SHORT).show()
+            }
+        }
+        binding.daftarakun.setOnClickListener() {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+    }
+
+    private fun loginUser(email: String, password: String) {
+        val requestQueue: RequestQueue = Volley.newRequestQueue(applicationContext)
+
+        val stringRequest = StringRequest(
+            Request.Method.GET,
+            "${DbContract.urlLogin}?email=$email&password=$password",
+            { response ->
+                Log.d("LoginActivity", "Server Response: $response")
+                if (response == "welcome") {
+                    Log.d("LoginActivity", "Login Successful")
+                    Toast.makeText(applicationContext, "Login successful", Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(applicationContext, MainActivity::class.java))
+                } else {
+                    Log.d("LoginActivity", "Login Failed")
+                    Toast.makeText(applicationContext, "Login failed", Toast.LENGTH_SHORT).show()
+                }
+            },
+            { error ->
+                Log.e("LoginActivity", "Volley Error: ${error.message}")
                 Toast.makeText(
-                    this,
-                    "Login gagal. Periksa username dan password.",
+                    applicationContext,
+                    "Login Failed: ${error.message}",
                     Toast.LENGTH_SHORT
                 ).show()
             }
-        }
+        )
+        requestQueue.add(stringRequest)
     }
-}
 
-//    private lateinit var binding: ActivityLoginBinding
-//    private val etUsername: EditText by lazy { binding.etEmail }
-//    private val etPassword: EditText by lazy { binding.etKataSandi }
-//        override fun onCreate(savedInstanceState: Bundle?) {
-//            super.onCreate(savedInstanceState)
-//            binding = ActivityLoginBinding.inflate(layoutInflater)
-//            setContentView(binding.root)
-//            supportActionBar?.hide()
-//
-//            binding.tombolMasuk.setOnClickListener {
-//                val username = etUsername.text.toString()
-//                val password = etPassword.text.toString()
-//
-//                if (!(username.isEmpty() || password.isEmpty())) {
-//                    loginUser(username, password)
-//                } else {
-//                    Toast.makeText(applicationContext, "Fields cannot be empty", Toast.LENGTH_SHORT).show()
-//                }
-//            }
-//        }
-//
-//        private fun loginUser(username: String, password: String) {
-//            val requestQueue: RequestQueue = Volley.newRequestQueue(applicationContext)
-//
-//            val stringRequest = StringRequest(
-//                Request.Method.GET,
-//                "${DbContract.urlLogin}?email=$username&password=$password",
-//                { response ->
-//                    Log.d("LoginActivity", "Server Response: $response")
-//                    if (response == "welcome") {
-//                        Log.d("LoginActivity", "Login Successful")
-//                        Toast.makeText(applicationContext, "Login successful", Toast.LENGTH_SHORT).show()
-//                        startActivity(Intent(applicationContext, MainActivity::class.java))
-//                    } else {
-//                        Log.d("LoginActivity", "Login Failed")
-//                        Toast.makeText(applicationContext, "Login failed", Toast.LENGTH_SHORT).show()
-//                    }
-//                },
-//                { error ->
-//                    Log.e("LoginActivity", "Volley Error: ${error.message}")
-//                    Toast.makeText(
-//                        applicationContext,
-//                        "Login Failed: ${error.message}",
-//                        Toast.LENGTH_SHORT
-//                    ).show()
-//                }
-//            )
-//            requestQueue.add(stringRequest)
-//        }
-//    }
+}
