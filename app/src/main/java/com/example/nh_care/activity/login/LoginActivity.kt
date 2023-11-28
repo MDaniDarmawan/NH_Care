057ae9db1c73bb6134e0a61013038627ae6ac3ec
farhanmahbubi <farhanmahbuby99@gmail.com>
2023-11-28 14:59:28 +0700

    login bisa + SharedPreferences

--- app/src/main/java/com/example/nh_care/activity/login/LoginActivity.kt
+++ app/src/main/java/com/example/nh_care/activity/login/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.nh_care.activity.login
 import android.os.Bundle
 import android.widget.Toast
 import android.content.Intent
+import android.content.SharedPreferences
 import android.util.Log
 import androidx.activity.ComponentActivity
 import com.android.volley.Request
@@ -12,10 +13,12 @@ import com.android.volley.toolbox.Volley
 import com.example.nh_care.activity.MainActivity
 import com.example.nh_care.activity.register.RegisterActivity
 import com.example.nh_care.databinding.ActivityLoginBinding
+import org.json.JSONException
+import org.json.JSONObject
+import java.net.URLEncoder
 
 class LoginActivity : ComponentActivity() {
     private lateinit var binding: ActivityLoginBinding
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,21 +30,29 @@ class LoginActivity : ComponentActivity() {
             startActivity(intent)
         }
 
-        val url = "http://192.168.1.12/api-mysql-main/api-login.php" //
+        val url = "http://192.168.1.70/api-mysql-main/api-login.php"
 
         binding.tombolMasuk.setOnClickListener {
             val request: RequestQueue = Volley.newRequestQueue(applicationContext)
 
             val stringRequest = StringRequest(
                 Request.Method.GET,
-                "$url?email=${binding.etEmail.text}&password=${binding.etKataSandi.text}",
+                "$url?email=${URLEncoder.encode(binding.etEmail.text.toString(), "UTF-8")}&password=${URLEncoder.encode(binding.etKataSandi.text.toString(), "UTF-8")}",
                 { response ->
-                    if (response == "welcome") {
-                        val intent = Intent(this, MainActivity::class.java)
-                        binding.etEmail.text.toString()
-                        startActivity(intent)
-                    } else {
-                        Toast.makeText(applicationContext, "Gagal login", Toast.LENGTH_LONG).show()
+                    try {
+                        val jsonResponse = JSONObject(response)
+                        val status = jsonResponse.getString("status")
+                        if (status == "success") {
+                            val idDonatur = jsonResponse.getString("id_donatur")
+                            saveID(idDonatur)
+
+                            val intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(applicationContext, "Gagal login", Toast.LENGTH_LONG).show()
+                        }
+                    } catch (e: JSONException) {
+                        Log.e("JSONError", "Error parsing JSON", e)
                     }
                 },
                 { error ->
@@ -50,5 +61,12 @@ class LoginActivity : ComponentActivity() {
             )
             request.add(stringRequest)
         }
+    }
+
+    private fun saveID(idDonatur: String) {
+        val preferences: SharedPreferences = getSharedPreferences("donatur_prefs", MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = preferences.edit()
+        editor.putString("id_donatur", idDonatur)
+        editor.apply()
     }
 }
